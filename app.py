76493e960b08d1c09c9612570b8233c1f07c1c32
Patrick Sontag <pmsontag84@gmail.com>
2022-08-04 11:12:22 -0500

    add pet

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, Pet
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -26,3 +27,36 @@ def home_page():
     pets = Pet.query.all()
     
     return render_template('home.html', pets=pets)
+
+@app.route('/add', methods=["GET", "POST"])
+def add_pet():
+    """Shows new pet form"""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        # data = {k: v for k, v in form.data.items() if k != "csrf_token"}
+        # new_pet = Pet(**data)
+        name = form.name.data
+        species = form.species.data
+        photo = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+        new_pet= Pet(name=name, species=species, photo_url=photo, age=age, notes=notes)
+
+        db.session.add(new_pet)
+        db.session.commit()
+
+        flash(f"Added {name} the {species}!")
+        return redirect("/")
+
+    else:
+        return render_template('add_pet.html', form=form)
+
+@app.route('/edit_pet/<int:pet_id>')
+def edit_pet_form(pet_id):
+    """Shows pets list"""
+
+    pet = Pet.query.got_or_404(pet_id)
+    
+    return render_template('edit_pet.html', pet=pet)
--- forms.py
+++ forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField, BooleanField
-from wtforms.validators import InputRequired, Optional
+from wtforms import StringField, FloatField, BooleanField, SelectField
+from wtforms.validators import InputRequired, Optional, URL, NumberRange
 
 class AddPetForm(FlaskForm):
     """Form for adding snacks."""
 
     name = StringField("Pet Name", validators=[InputRequired()])
-    species = StringField("Species", validators=[InputRequired()])
-    photo_url = StringField("Image", validators=[Optional()])
-    age = FloatField("Age", validators=[Optional()])
+    species = SelectField("Species", choices=[('dg','dog'), ('ct','cat'), ('pc', 'porcupine')])
+    photo_url = StringField("Image", validators=[Optional(), URL()])
+    age = FloatField("Age", validators=[Optional(), NumberRange(min=0, max=30)])
     notes = StringField("Notes", validators=[Optional()])
-    available = BooleanField("Available", validators=[InputRequired()])
+    # available = BooleanField("Available", validators=[InputRequired()])
